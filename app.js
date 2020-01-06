2dc742b43df6232c487f84ad2ee7364164b59908
LawrenceDavy13 <lawrencedavy13@gmail.com>
2020-01-06 22:33:16 +0000

    Start of notes application

---
+++ app.js
@@ -0,0 +1,9 @@
+// Import libraries
+const getNotes = require('./notes')
+
+const msg = getNotes()
+
+// Print variable to the console
+console.log(msg)
+
+
---
+++ notes.js
@@ -0,0 +1,10 @@
+// Check to see if import is working
+console.log('notes.js')
+
+// Created variable
+const getNotes = function() {
+    return "Your notes"
+}
+
+// Export variables to used by other file
+module.exports = getNotes
